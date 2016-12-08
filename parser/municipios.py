9c681b6b7fef332e90664ac64b9c201ee8feef85
IagoLast <iago.lastra@gmail.com>
2016-12-08 14:02:07 +0100

    add new data files and data parser

---
+++ parser/municipios.py
@@ -0,0 +1,50 @@
+import xlrd
+import json
+
+
+def xls_to_json(filename):
+    doc = xlrd.open_workbook(filename)
+    sheet = doc.sheet_by_index(0)
+    nrows = sheet.nrows
+    municipios = []
+    id = 0
+
+    if not is_valid(sheet):
+        print(
+            "Error en {0}, el archivo tiene un formato inesperado.".format(filename))
+
+    for row in range(3, nrows):
+        id += 1
+        cod_mun = sheet.cell_value(row, 2)
+        name_mun = sheet.cell_value(row, 4)
+
+        municipios.append({
+            'id': id,
+            'nm': name_mun,
+            'pv': cod_mun
+        })
+    with open('municipios.json', 'wb') as fp:
+        json_string = json.dumps(municipios, ensure_ascii=False).encode('utf8')
+        fp.write(json_string)
+
+
+def is_valid(sheet):
+    """Check if the given datasheet has the expected format."""
+    if str(sheet.cell_value(1, 0)) != "CODAUTO":
+        print "Se esperaba CPRO y se tiene {0}".format(sheet.cell_value(1, 0))
+        return False
+    if str(sheet.cell_value(1, 1)) != "CPRO":
+        print "Se esperaba CPRO y se tiene [{0}]".format(sheet.cell_value(1, 1))
+        return False
+    if str(sheet.cell_value(1, 2)) != "CMUN":
+        print "Se esperaba CMUN y se tiene {0}".format(sheet.cell_value(1, 2))
+        return False
+    if str(sheet.cell_value(1, 3)) != "DC":
+        print "Se esperaba DC y se tiene {0}".format(sheet.cell_value(1, 3))
+        return False
+    if str(sheet.cell_value(1, 4)) != "NOMBRE":
+        print "Se esperaba NOMBRE y se tiene {0}".format(sheet.cell_value(1, 4))
+        return False
+    return True
+
+xls_to_json('16codmun.xlsx')
---
+++ parser/provincias.py
@@ -0,0 +1,39 @@
+import xlrd
+import json
+
+
+def xls_to_json(filename):
+    doc = xlrd.open_workbook(filename, encoding_override='cp1252')
+    sheet = doc.sheet_by_index(0)
+    nrows = sheet.nrows
+    municipios = []
+
+    if not is_valid(sheet):
+        print(
+            "Error en {0}, el archivo tiene un formato inesperado.".format(filename))
+        print 1
+    for row in range(1, nrows):
+        print row
+        code_prov = sheet.cell_value(row, 2)
+        name_prov = sheet.cell_value(row, 3)
+
+        municipios.append({
+            'id': code_prov,
+            'nm': name_prov
+        })
+    with open('provincias.json', 'wb') as fp:
+        json_string = json.dumps(municipios, ensure_ascii=False).encode('utf8')
+        fp.write(json_string)
+
+
+def is_valid(sheet):
+    """Check if the given datasheet has the expected format."""
+    if str(sheet.cell_value(0, 2)) != "CPRO":
+        print "Se esperaba CPRO y se tiene [{0}]".format(sheet.cell_value(0, 2))
+        return False
+    if str(sheet.cell_value(0, 3)) != "NOMBRE":
+        print "Se esperaba NOMBRE y se tiene {0}".format(sheet.cell_value(0, 3))
+        return False
+    return True
+
+xls_to_json('16codprov.xlsx')
